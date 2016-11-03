e5576af4cb5613f9ecb70bd8234004a26b5b8589
adrienlementheour <adrien.le.mentheour@gmail.com>
2016-11-03 22:10:59 +0100

    :art: Icons

--- src/theme/footer.php
+++ src/theme/footer.php
@@ -1,4 +1,23 @@
 	<footer role='contentinfo'>
+		<div class='container'>
+			<ul>
+				<li>
+					<h2>S’inscrire à la newsletter</h2>
+					<form id='newsletter'>
+						<fieldset>
+							<input type='email' name='search' placeholder='Votre adresse email...'>
+							<button>S'inscrire</button>
+						</fieldset>
+					</form>
+				</li><li>
+					<h2>Société</h2>
+				</li><li>
+					<h2>Besoin d’aide ?</h2>
+				</li><li>
+					
+				</li>
+			</ul>
+		</div>
 	</footer>
 
 	<?php wp_footer(); ?>
